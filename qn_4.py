625a196d8497e1cbc7dc6cdc0397f939d8a0f20d
ameeshakumari <95542204+ameeshakumari@users.noreply.github.com>
2021-12-04 23:29:42 +0530

    Add files via upload

---
+++ qn_4.py
@@ -0,0 +1,22 @@
+# basic_salary=int(input("enter the number"))
+# gross_salary=int(input("enter the number"))
+# if basic_salary<=100000:
+#      print("HAR=20%")     
+#     print("DA =80%")
+# elif basic_salary<=20000:
+#     print("HRA=25%") 
+#     print("DA=90%") 
+# elif basic_salary>20000:
+#     print("HRA=30%") 
+#     print("DA=95%") 
+# else:
+#     print("Gross salary")  
+
+
+    
+    
+    
+
+
+        
+      
