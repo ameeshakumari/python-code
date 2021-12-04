bc5df4d6995e3452952df7a38a76b7db26b8a0dc
ameeshakumari <95542204+ameeshakumari@users.noreply.github.com>
2021-12-04 23:29:16 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,80 @@
+# num1=int(input("enter the number"))
+# num2=int(input("enter the number"))
+# if num1<num2:
+#     print("num1 se bada num2 hai")
+# elif num2<num1:
+#     print("yeh num1 chotha hai")
+# else:
+#     print("nothing")  
+    
+    
+# month=int(input("enter the number"))
+# if month==31:
+#     print("january") 
+# elif month==28:
+#     print("february")
+# elif month==31:
+#     print("march")
+# elif month==30:
+#     print("april")
+# elif month==31:
+#     print("may")
+# elif month==30:
+#     print("june") 
+# elif month==31:
+#     print("july") 
+# elif month==31:
+#     print("august")
+# elif month==30:
+#     print("septemer")
+# else:
+#     print("nothing")                  
+                      
+# month=input("enter the month......")
+# if month=="janury" or month=="march" or month=="may" or month=="july" or month=="august" or month=="october" or month=="december":
+#     print("31")
+# elif month=="april" or month=="june" or month=="september" or month=="november":      
+#     print("30") 
+# elif month=="febuary" or month=="febuary":
+#     print("28 ,29") 
+# else:
+#     pa
+
+
+# physics= int(input("enter the physic marks"))
+# chemistry=int(input("enter the chemistry mark"))
+# biology=int(input("enter the biology mark "))
+# mathematics=intinput("enter the mathematics mark"))
+# computer=int(input("enter the computer mark"))
+# total=physic+chemistry+biology+mathematics+computer    
+# if percentage>=90:
+#     print("grade A")
+# elif percentage>=80:
+#     print("grade B")
+# elif percentage>=70:
+#     print("grade C")
+# elif percentage/500*100:
+#     print("per",percentage)
+#     print("grade D")
+# elif percentage>=40:
+#     print("grade E")
+# elif percentage>=30:
+#     print("fail")
+# else:
+#     print(nothing)
+
+       
+                          
+        
+                
+
+
+
+
+
+
+
+
+
+
+          
