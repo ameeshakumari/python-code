317350f6d306202ddf89d25e36263b82caef07e3
ameeshakumari <95542204+ameeshakumari@users.noreply.github.com>
2021-12-04 23:00:53 +0530

    Add files via upload

---
+++ qn_6.py
@@ -0,0 +1,16 @@
+if b==b:
+    print("ball")
+if c==c:
+    print("cat")
+if d==d:
+    print("dog") 
+if e==e:
+    print("eat") 
+if f==f:
+    print("fan") 
+if g==g:
+    print("gan") 
+if h==h:
+    print("hen") 
+if l==l:
+    print("life")-                        
